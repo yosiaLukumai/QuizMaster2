05283d6504ec9603eb451fae7eb55f616f19bc52
fab-ani <111348845+fab-ani@users.noreply.github.com>
2024-03-09 12:13:55 -0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ["**/*.JPG"],
+   esbuild: {
+    supported: {
+      'top-level-await': true //browsers can handle top-level-await features
+    },
+  }
 })
